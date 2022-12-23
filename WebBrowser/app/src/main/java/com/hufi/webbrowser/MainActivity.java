1ae3f07cd1a69b1c81371f65283212ae8c589fcf
TranGiaPhucc <shiroineko1999@gmail.com>
2022-12-23 12:18:41 +0700

    Add adblock

--- WebBrowser/app/src/main/java/com/hufi/webbrowser/MainActivity.java
+++ WebBrowser/app/src/main/java/com/hufi/webbrowser/MainActivity.java
@@ -31,6 +31,7 @@ import android.webkit.DownloadListener;
 import android.webkit.URLUtil;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.AdapterView;
@@ -49,7 +50,9 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     SwipeRefreshLayout refreshLayout;
@@ -572,6 +575,42 @@ public class MainActivity extends AppCompatActivity {
                         throwables.printStackTrace();
                     }*/
                 }
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                /*if (url.endsWith(".mp4")) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setDataAndType(Uri.parse(url), "video/*");
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    view.getContext().startActivity(intent);
+                    return true;
+                } else*/
+                    if (url.startsWith("tel:") || url.startsWith("sms:") || url.startsWith("smsto:")
+                        || url.startsWith("mms:") || url.startsWith("mmsto:")) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    view.getContext().startActivity(intent);
+                    return true;
+                } else {
+                    return super.shouldOverrideUrlLoading(view, url);
+                }
+            }
+
+            private Map<String, Boolean> loadedUrls = new HashMap<>();
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+                boolean ad;
+                if (!loadedUrls.containsKey(url)) {
+                    ad = AdBlocker.isAd(url);
+                    loadedUrls.put(url, ad);
+                } else {
+                    ad = loadedUrls.get(url);
+                }
+                return ad ? AdBlocker.createEmptyResource() :
+                        super.shouldInterceptRequest(view, url);
             }
         });
 
